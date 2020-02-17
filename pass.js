bb82754ab77e66d58ce390854c83b7b6fb3973cf
Matt <imel.matt.r@gmail.com>
2020-02-17 17:47:38 -0600

    Temporary fix; need to hide password later.

---
+++ pass.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql');
+
+module.exports = mysql.createConnection({
+  host: 'kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  database: 'ga690xess9dv54xr',
+  user: 'lrw96hlzv0vts9qt',
+  password: 'as18bkyhuxlqih29'
+});
